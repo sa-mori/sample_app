bb981168d511643fc4ef9ddb522a10004a94772c
s-mori <pillows77@gmail.com>
2017-07-17 04:44:47 +0200

    add test flash

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -17,4 +17,19 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.field_with_errors' 
     assert_select 'form[action="/signup"]'
   end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count',1 do
+      post signup_path,params:{user: { name: "Example User",
+                                      email: "user@exampke.com",
+                                      password: "password",
+                                      password_confirmation: "password"
+                                    }
+                             }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not_nil(flash)
+  end
 end
