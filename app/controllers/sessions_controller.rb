6df8092eb3547f4192a60086e58302466b443155
s-mori <pillows77@gmail.com>
2017-07-19 07:16:25 +0200

    add session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email:params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      login user
+      redirect_to user
+    else
+      # エラーメッセージを作成する
+      flash.now[:danger] = "Invalid email/password combition"
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
