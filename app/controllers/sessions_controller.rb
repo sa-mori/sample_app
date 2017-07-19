4cced5a3088b21429475989ef458f05b605d0846
s-mori <pillows77@gmail.com>
2017-07-19 11:44:50 +0200

    Implement basic login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email:params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      login user
+      log_in user
       redirect_to user
     else
       # エラーメッセージを作成する
@@ -15,5 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,6 @@
 module SessionsHelper
 
-  def login(user)
+  def log_in(user)
     session[:user_id] = user.id
   end
 
@@ -10,6 +10,11 @@ module SessionsHelper
 
   def logged_in?
     !current_user.nil?
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user=nil
   end
 
 end
