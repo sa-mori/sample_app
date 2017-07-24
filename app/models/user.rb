ff0d6afa1bd037412198d0569d7f56ebd8ba80a3
s-mori <pillows77@gmail.com>
2017-07-24 07:05:23 +0200

    Add remember token to cookies

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,10 @@ class User < ApplicationRecord
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
+
+  # 渡されたトークンがダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remeber_token)
+  end
+
 end
