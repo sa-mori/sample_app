302862cb9cce8454e711f86939568fdd0015e2e3
s-mori <pillows77@gmail.com>
2017-08-04 16:45:11 +0200

    Edit bug

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -21,12 +21,14 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   test "successful edit" do
     get edit_user_path(@user)
     assert_template 'users/edit'
+    name  = "Foo Bar"
+    email = "foo@bar.com"
     patch user_path(@user), params: { user: { name:  name,
                                               email: email,
                                               password:  "",
                                               password_confirmation: "" } }
-    assert_not flash.emtpy?
-    assert_redirect_to @user
+    assert_not flash.empty?
+    assert_redirected_to @user
     @user.reload
     assert_equal name, @user.name
     assert_equal email, @user.email
