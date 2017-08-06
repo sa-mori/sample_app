99760584b950f6bf19a86867ad710d59a8527aeb
s-mori <pillows77@gmail.com>
2017-08-06 02:54:30 +0200

    Add users/index layout test

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -19,4 +19,15 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", contact_path
   end
 
+  def setup
+    @user = users(:michael)
+  end
+
+  test "user layout links" do
+    log_in_as(@user)
+    get users_path
+    assert_template 'users/index'
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+  end
 end
