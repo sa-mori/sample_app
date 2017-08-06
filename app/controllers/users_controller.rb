126ab05a43e221ef0b42d93b47ecefa31349b537
s-mori <pillows77@gmail.com>
2017-08-06 14:22:28 +0200

    Add delete action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
   # 編集される前にログイン確認
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
 
   # 編集される前に対象ユーザか確認
   before_action :correct_user, only: [:edit, :update]
+
+  # destroyアクションはadminのみ
+  before_action :admin_user, only: :destroy
 
   def show
     @user = User.find(params[:id])
@@ -42,6 +45,12 @@ class UsersController < ApplicationController
     @users = User.paginate(page: params[:page],per_page: 10)
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
   private
 
     def user_params
@@ -62,6 +71,9 @@ class UsersController < ApplicationController
   def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
-    end
- 
+   end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end 
 end
