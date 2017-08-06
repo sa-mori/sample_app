03ce015a107ad768fa9d67c07cf237a62f04741c
s-mori <pillows77@gmail.com>
2017-08-06 13:19:49 +0200

    Add fixture many user

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -38,7 +38,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 
   test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
-    assert_nil session[:forwarding_url]
+    assert_not_nil session[:forwarding_url]
     log_in_as(@user)
     assert_redirected_to edit_user_url(@user)
     name = "Foo bar"
